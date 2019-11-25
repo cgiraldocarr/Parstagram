7e2173bdc9b2586a6ab3943a0ffa83305c8fa245
cgiraldocarr <37774190+cgiraldocarr@users.noreply.github.com>
2019-11-25 02:53:10 -0500

    new update

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Catherine Giraldo on 11/25/19.
+//  Copyright © 2019 Catherine Giraldo. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
